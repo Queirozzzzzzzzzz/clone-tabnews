dc19c67ecc4c4517e3638ebb9bd35f8c6f3802dc
Queirozzzzzzzzzz <141048057+Queirozzzzzzzzzz@users.noreply.github.com>
2023-08-27 17:34:57 +0000

    arrumando a estrutura do pages

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,5 @@
 function Home() {
-    return `
-        <!DOCTYPE html>
-        <html lang="pt-br">
-        <head>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <link rel="stylesheet" href="style.css">
-            <title>Opa</title>
-        </head>
-        <body>
-            <button onclick="location.href='https://www.youtube.com/watch?v=dQw4w9WgXcQ';">Clique aqui</button>
-        </body>
-        </html>
-        `;
+    return <button onclick="location.href='https://www.youtube.com/watch?v=dQw4w9WgXcQ';">Clique aqui</button>;
 }
 
 export default Home;
