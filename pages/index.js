e491af7990bf03c5b3edefe17181966494b70671
Queirozzzzzzzzzz <141048057+Queirozzzzzzzzzz@users.noreply.github.com>
2023-08-27 16:35:13 +0000

    Modifica o pages novamente

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
 function Home() {
-    return <input type="button" onclick="location.href='https://www.youtube.com/watch?v=dQw4w9WgXcQ';" value="Clique" />
+    return (
+        <input type="button" value="Clique" onclick="location.href='https://www.youtube.com/watch?v=dQw4w9WgXcQ';" />
+    );
 }
 export default Home;
