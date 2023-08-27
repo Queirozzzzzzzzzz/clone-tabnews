b3042b2c562633029ae4c44c42ef4868a6f24005
Queirozzzzzzzzzz <141048057+Queirozzzzzzzzzz@users.noreply.github.com>
2023-08-27 17:45:19 +0000

    reestruturando o pages

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <button onclick="location.href='index.html';">Clique aqui</button>;
+    return <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">Clique aqui</a>
 }
 
 export default Home;
